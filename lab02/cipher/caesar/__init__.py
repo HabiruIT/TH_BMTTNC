6a113bd1fc0badc0ab393abc4ddf0a74410d2618
HabiruIT <dangthinhdaiit@gmail.com>
2025-03-05 10:52:53 +0700

    lab02

---
+++ lab02/cipher/caesar/__init__.py
@@ -0,0 +1,2 @@
+from .alphabet import BANGCHUCAI
+from.caesar_cipher import CaesarCipher
---
+++ lab02/cipher/railfence/railfence_cipher.py
@@ -0,0 +1,6 @@
+class RailRenceCipher:
+    def __init__(self):
+        pass
+
+    def rail_fence_encrypt(self, plain_text, num_rails):
+        
